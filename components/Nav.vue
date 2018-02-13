a577e5c7df76c14837e4b67ac7f0a69ff4e45359
pochis852 <pochis43@yahoo.es>
2018-02-13 19:30:21 +0000

    MASTER working on layout

---
+++ components/Nav.vue
@@ -0,0 +1,75 @@
+<template>
+    <nav>
+      <v-navigation-drawer
+          fixed
+          clipped
+          v-model="drawer"
+          :mini-variant="mini"
+          dark 
+          app>
+      <v-list>
+        <v-list-tile v-if="mini" @click="mini = !mini">
+          <v-list-tile-action>
+            <v-icon>chevron_right</v-icon>
+          </v-list-tile-action>
+        </v-list-tile>
+        <v-list-tile avatar tag="div">
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg" >
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>Andres Arbelaez Acevedo</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-action>
+            <v-btn icon @click.stop="mini = !mini">
+              <v-icon>chevron_left</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+      <v-list dense>
+        <v-divider light></v-divider>
+        <v-list-tile v-for="menu in menuItems" :key="menu.title">
+          <v-list-tile-action>
+            <v-icon>{{ menu.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ menu.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+        <v-toolbar color="primary" height="45px" app>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title class="white--text">Ticketeando</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon class="white--text">
+                <v-badge color="accent">
+                  <span slot="badge">6</span>
+                      <v-icon>{{(hasNotification ? 'notifications_active':'notifications')}}</v-icon>
+                </v-badge>
+            </v-btn>
+            <v-btn icon class="white--text">
+              <v-icon>more_vert</v-icon>
+            </v-btn>
+             <v-avatar :tile=false size="40px"class="grey lighten-4">
+              <img src="/v.png" alt="username" title="username">
+            </v-avatar>
+        </v-toolbar>
+    </nav>
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                hasNotification:false,
+                drawer: false,
+                menuItems: [
+                  { title: 'Home', icon: 'dashboard' },
+                  { title: 'About', icon: 'question_answer' }
+                ],
+                mini: true,
+            }
+        }
+    }
+</script>
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -4,12 +4,12 @@ import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#121212', // a color that is not in the material colors palette
-    accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
-    info: colors.teal.lighten1,
-    warning: colors.amber,
-    error: colors.deepOrange.accent4,
-    success: colors.green.accent3
+      primary: "#8D6E63",
+      secondary: "#5D4037",
+      accent: "#F44336",
+      error: "#f44336",
+      warning: "#ffeb3b",
+      info: "#2196f3",
+      success: "#4caf50"
   }
 })
